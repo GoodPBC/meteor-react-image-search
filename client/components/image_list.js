d113c7bf1a0e584a057f3c657c8cd6c7d6a95d38
GoodPBC <admin@goodygiver.com>
2017-07-01 11:20:54 -0400

    create static images data for list view(placeholders)

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -3,7 +3,14 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
+//static data source for our images
+const IMAGES = [
+  {title: 'Joe', link: 'https://dummyimage.com/200x150/00bbf5/000.jpg&text=image'},
+  {title: 'Aleks', link: 'https://dummyimage.com/200x150/00bbf5/000.jpg&text=image'},
+  {title: 'Godfrey', link: 'https://dummyimage.com/200x150/00bbf5/000.jpg&text=image'},
+  {title: 'Jacek', link: 'https://dummyimage.com/200x150/00bbf5/000.jpg&text=image'}
 
+]
 //CREATE OUR IMAGE LIST component
 const ImageList = () => {
   return (
