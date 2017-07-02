053206ee8c367ff3268aa35f285d594435404ead
GoodPBC <admin@goodygiver.com>
2017-07-02 13:00:45 -0400

    console logging response object from imgur API

--- client/app.js
+++ client/app.js
@@ -14,15 +14,13 @@ const App = () => {
 }
 //anyhting inside of this function will be fired after our app has loaded
 Meteor.startup( () =>{
-  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+
   //render a component
   //<App /> is an instance of the App component that we just created
   ReactDOM.render(<App />, document.querySelector('.container'));  //use the react dom library--to render--the app component -- to some HTML element with class container
+
+  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+  .then(response => console.log(response));
+
 
 });
