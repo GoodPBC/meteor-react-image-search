d26b4b75fd0938c7ae6fab57cc26eba9ea03e67a
GoodPBC <admin@goodygiver.com>
2017-07-02 12:51:19 -0400

    installed and imported axios

--- client/app.js
+++ client/app.js
@@ -1,7 +1,9 @@
 // destructured react import
 import React from 'react';
 import ReactDOM from 'React-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
+
 // create a component
 const App = () => {
   return (
@@ -12,6 +14,13 @@ const App = () => {
 }
 //anyhting inside of this function will be fired after our app has loaded
 Meteor.startup( () =>{
+  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
   //render a component
   //<App /> is an instance of the App component that we just created
   ReactDOM.render(<App />, document.querySelector('.container'));  //use the react dom library--to render--the app component -- to some HTML element with class container
