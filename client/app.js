f8a986e9d8f935b9096c80145905b49693a74e0e
GoodPBC <admin@goodygiver.com>
2017-07-07 22:45:56 -0400

    refactored App component for state.

--- client/app.js
+++ client/app.js
@@ -6,12 +6,18 @@ import ImageList from './components/image_list';
 
 // create a component
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { images: [] };
+  }
   componentWillMount() { //great place to load data
     axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-    .then(response => console.log(response));
+    .then(response => this.setState({ images: response.data.data }));
   }
 
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <ImageList />
